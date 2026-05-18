22783334ff0aeb3b77f6394759f4c98015566f38
CesarSalimbeni <cesar.salimbeni@correo.unimet.edu.ve>
2026-05-18 17:50:02 -0400

    Sistema de logica de buscaminas
    En este commint, se envía la clase buscaminas con sus funciones y datos para integrarla a la interfaz.

---
+++ getionEstados.dart
@@ -0,0 +1,6 @@
+/*Esta clase esta destinada a manejar el cronometro, además de guardar los intentos, records de tiempos e intentos 
+(solo 5 marcas y por difilcultad).
+Tambien borrar los marcadores.
+Todo esto para ser guardado junto con la preferencia de datos.
+*/
+
